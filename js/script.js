4bd7ea35d5edb1774ca40bac98d1beaa9918287c
Rachelle <rmbennin@iupui.edu>
2013-10-24 17:15:55 -0400

    jQuery
    Added some jQuery interactivity. Name now generates in the top left
    from the first screen. Second screen allows you to click a level and go
    to that level's function (filled with random text temporarily).

--- js/script.js
+++ js/script.js
@@ -1,5 +1,59 @@
-$(document).ready(function(){
+// ==================================================NAMES SCREEN==================================================
+
+function nameScreen(){
+			
+	$.get("nameScreen.html", function(data){
+		$("#gameContent").html(data);
+	});
 	
-	
+} //end nameScreen function
+
+$("#nextButton").click(generateName);
 		
-})
+function generateName(){
+	
+	$("#userName").html($("#form-name").val());
+	levelsScreen();
+	
+} //end generateName function
+
+// ==================================================LEVELS SCREEN==================================================
+
+function levelsScreen(){
+			
+	$.get("levelsScreen.html", function(data){
+		$("#gameContent").html(data);
+	});
+	
+} //end levelsScreen function
+	
+$("#easyLevel").click(easyLevel);
+$("#mediumLevel").click(mediumLevel);
+$("#hardLevel").click(hardLevel);
+$("#twoPlayerLevel").click(twoPlayerLevel);
+
+// ==================================================LEVELS==================================================
+
+function easyLevel(){
+		
+	$("#gameContent").html("<h2>easy level</h2>");
+	
+} //end easyLevel function
+
+function mediumLevel(){
+	
+	$("#gameContent").html("<h2>medium level</h2>");
+	
+} //end mediumLevel function
+
+function hardLevel(){
+	
+	$("#gameContent").html("<h2>hard level</h2>");
+	
+} //end hardLevel function
+
+function twoPlayerLevel(){
+	
+	$("#gameContent").html("<h2>two player level</h2>");
+	
+} //end twoPlayerLevel function
